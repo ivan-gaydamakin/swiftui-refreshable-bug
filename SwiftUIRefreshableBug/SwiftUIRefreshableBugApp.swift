664cc10aa57fae3e43f37960739a182b70a5a9d4
Ivan Gaydamakin <ivan.gaydamakin@wallester.com>
2021-09-23 14:35:28 +0300

    Initial Commit

---
+++ SwiftUIRefreshableBug/SwiftUIRefreshableBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIRefreshableBugApp.swift
+//  SwiftUIRefreshableBug
+//
+//  Created by Ivan Gaydamakin on 23.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIRefreshableBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
