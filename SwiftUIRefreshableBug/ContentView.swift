cbbc944559319b78766abfe0e1f1e716b71434a3
Ivan Gaydamakin <ivan.gaydamakin@wallester.com>
2021-09-23 14:46:44 +0300

    First commit

--- SwiftUIRefreshableBug/ContentView.swift
+++ SwiftUIRefreshableBug/ContentView.swift
@@ -8,9 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                VStack {
+                    Color.yellow.frame(width: 100, height: 200)
+                }
+            }.refreshable {
+                await raiseHand()
+            }
+            .navigationTitle("Navigation")
+            .navigationBarTitleDisplayMode(.inline) // if comment, no bugs
+            .toolbar(content: {
+                NavigationLink(destination: SecondView()) {
+                    Text("Show Detail View")
+                }
+            })
+        }
+    }
+    
+    private func raiseHand() async {
+        await withCheckedContinuation { continuation in
+            DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(11)) {
+                continuation.resume()
+            }
+        }
+    }
+}
+
+struct SecondView: View {
+    var body: some View {
+        VStack {
+            Text("This is the detail view")
+            Spacer()
+            Text("Some text")
+        }
+        .navigationTitle("Second")
     }
 }
 
